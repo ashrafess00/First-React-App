c22d3dcb70243b0e1da7b30fcb6de5f10acc2416
Achraf Essaoudi <achrafess1937@gmail.com>
2021-10-05 15:53:45 +0100

    s

--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,15 @@
 import React from "react";
 
-import facebook from "../src/images/icon-facebook.svg";
-import twitter from "../src/images/icon-twitter.svg";
-import pinterest from "../src/images/icon-pinterest.svg";
-import instagram from "../src/images/icon-instagram.svg";
+// import facebook from "../src/images/icon-facebook.svg";
+// import twitter from "../src/images/icon-twitter.svg";
+// import pinterest from "../src/images/icon-pinterest.svg";
+// import instagram from "../src/images/icon-instagram.svg";
+
+import { faFacebook } from "@fortawesome/free-brands-svg-icons";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faPinterest } from "@fortawesome/free-brands-svg-icons";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Footer = () => {
   return (
@@ -39,10 +45,14 @@ const Footer = () => {
       </div>
 
       <div>
-        <img src={facebook} alt="" className="icons" />
-        <img src={twitter} alt="" className="icons" />
+        {/* <img src={facebook} alt="" className="icons" /> */}
+        <FontAwesomeIcon icon={faFacebook} className="icons" />
+        <FontAwesomeIcon icon={faTwitter} className="icons" />
+        <FontAwesomeIcon icon={faPinterest} className="icons" />
+        <FontAwesomeIcon icon={faInstagram} className="icons" />
+        {/* <img src={twitter} alt="" className="icons" />
         <img src={pinterest} alt="" className="icons" />
-        <img src={instagram} alt="" className="icons" />
+        <img src={instagram} alt="" className="icons" /> */}
       </div>
     </footer>
   );
